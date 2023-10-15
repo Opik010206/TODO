e0328f45fe13b7c34a7c9384b3c7a360af76c478
Ainur Rofiq <rofik010206@gmail.com>
2023-10-15 22:38:20 +0700

    menambahkan localStorage, sessionStorage

--- js/script.js
+++ js/script.js
@@ -12,6 +12,34 @@
 const todos = [];
 const RENDER_EVENT = 'render-todo';
 
+const SAVED_EVENT = 'saved-todo';
+const STORAGE_KEY = 'TODO_APPS';
+ 
+function isStorageExist() /* boolean */ {
+  if (typeof (Storage) === undefined) {
+    alert('Browser kamu tidak mendukung local storage');
+    return false;
+  }
+  return true;
+}
+
+document.addEventListener(SAVED_EVENT, function () {
+  console.log(localStorage.getItem(STORAGE_KEY));
+});
+
+function loadDataFromStorage() {
+  const serializedData = localStorage.getItem(STORAGE_KEY);
+  let data = JSON.parse(serializedData);
+ 
+  if (data !== null) {
+    for (const todo of data) {
+      todos.push(todo);
+    }
+  }
+ 
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
 document.addEventListener(RENDER_EVENT, function () {
     const uncompletedTODOList = document.getElementById('todos');
     uncompletedTODOList.innerHTML = '';
@@ -52,6 +80,7 @@ function addTodo() {
     todos.push(todoObject);
    
     document.dispatchEvent(new Event(RENDER_EVENT));
+    saveData();
 }
 
 document.addEventListener('DOMContentLoaded', function(){
@@ -59,7 +88,11 @@ document.addEventListener('DOMContentLoaded', function(){
     submitForm.addEventListener('submit', function(event){
         event.preventDefault();
         addTodo();
-    })
+    });
+
+    if (isStorageExist()) {
+      loadDataFromStorage();
+    }
 });
 
 
@@ -117,6 +150,7 @@ function addTaskToCompleted (todoId) {
    
     todoTarget.isCompleted = true;
     document.dispatchEvent(new Event(RENDER_EVENT));
+    saveData();
 }
 
 function findTodo(todoId) {
@@ -138,16 +172,18 @@ function removeTaskFromCompleted(todoId) {
    
     todos.splice(todoTarget, 1);
     document.dispatchEvent(new Event(RENDER_EVENT));
-  }
+    saveData();
+}
    
    
-  function undoTaskFromCompleted(todoId) {
+function undoTaskFromCompleted(todoId) {
     const todoTarget = findTodo(todoId);
    
     if (todoTarget == null) return;
    
     todoTarget.isCompleted = false;
     document.dispatchEvent(new Event(RENDER_EVENT));
+    saveData();
 }
 
 function findTodoIndex(todoId) {
@@ -158,4 +194,12 @@ function findTodoIndex(todoId) {
     }
    
     return -1;
+}
+
+function saveData() {
+  if (isStorageExist()) {
+    const parsed = JSON.stringify(todos);
+    localStorage.setItem(STORAGE_KEY, parsed);
+    document.dispatchEvent(new Event(SAVED_EVENT));
+  }
 }
